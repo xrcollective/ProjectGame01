42952e74df2a2a1b0063e5f3e991135f2122eb2e
Akhil Menon <menonakhilmenon@gmail.com>
2019-08-14 12:55:49 +0530

    Added SteamVR and HoverMovement Script

---
+++ Project Game/Assets/ProjectGame/Scripts/HoverMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverMovement : MonoBehaviour
+{
+    //The maximum distance the head/joystick movement should be clamped to, setting this value lower will make the movement more sensitive
+    public float axisClampDistance = 1f;
+    //The object which acts as a joystick axis in VR typically the head for hoverboard like movement
+    public Transform head;
+
+    private Vector3 currPos;
+
+    public Vector3 movementVector;
+    private void Move()
+    {
+        movementVector = head.localPosition - currPos;
+        movementVector.x = Mathf.Clamp(movementVector.x / axisClampDistance, -1, 1);
+        movementVector.z = Mathf.Clamp(movementVector.z / axisClampDistance, -1, 1);
+        movementVector.y = 0;
+    }
+    public void SetPos()
+    {
+        currPos = head.localPosition;
+    }
+    public void ResetPos()
+    {
+        movementVector = Vector3.zero;
+    }
+
+    private void OnDisable()
+    {
+        ResetPos();
+    }
+}
